af9169d523af36c5bad8e02dc6a6882e1d901d45
alyaskastorm <minealaska29@gmail.com>
2021-08-07 23:15:51 +0300

    Add some handlers

--- src/handlers/handler.cr
+++ src/handlers/handler.cr
@@ -1,10 +1,33 @@
-get "/:user" do |env|
+get "/user/:user" do |env|
     user = env.params.url["user"]
     render "src/views/user.ecr", "src/views/layouts/layout.ecr"
 end
 
 get "/" do
     render "src/views/home.ecr", "src/views/layouts/layout.ecr"
+end
+
+get "/create_user" do
+    render "src/views/create_user.ecr", "src/views/layouts/layout.ecr" 
+end
+
+post "/create_user" do |env|
+    name = env.params.body["name"]? 
+    if name.nil?
+        next "400"
+    end
+
+    email = env.params.body["email"]? 
+    if email.nil?
+        next "400"
+    end
+
+    pwd = env.params.body["pswd"]?
+    if pwd.nil?
+        next "400"
+    end
+
+    "200"
 end
 
 error 404 do
--- src/server.cr
+++ src/server.cr
@@ -6,5 +6,6 @@ require "./views/*"
 module Server
   VERSION = "0.1.0"
   port = 5010
+
   Kemal.run port 
 end
